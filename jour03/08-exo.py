d4f362724ac936a1bd2c25800b90a9142b939447
Malik H <malik.h@webdevpro.net>
2024-10-02 12:34:01 +0200

    correction

---
+++ jour03/08-exo.py
@@ -0,0 +1,18 @@
+a = 2 
+b = 3 
+c = -5 
+
+verif1 =  a < b 
+#True
+print(verif1) 
+
+verif2 = a < b and b > c 
+#True et True
+#True
+print(verif2) 
+
+verif3 = a > b or b > c 
+#False  ou True 
+#True 
+
+print(verif3)
