18b03115e0196b6188326ad7869809f8b7694f39
Malik H <malik.h@webdevpro.net>
2024-10-02 12:21:58 +0200

    correction

---
+++ jour03/07-exo.py
@@ -0,0 +1,30 @@
+print( 2 >= 2) # True
+print("a" == "ab") # False
+print(2 != 3 and 10 < 33)
+ #     True  and  True
+ #         True
+print("hello" > "bonjour" )
+ # True 
+ # h => 68 hexadecimal
+ # b => 62 hexadecimal
+ #  68 > 62 
+print(2 ==  "2")
+# False # attention dans certain langage JS et PHP => True 
+
+print("2" == "2")
+# True
+print((2 != 5 and 3 > 4 ) or 2 <= 14 )
+#       (True  and False) or True
+#                   False or True
+#                       True
+
+print(2 != 5 and ( 3 > 4  or 2 <= 14 ) )
+#      True  and (False    or True)
+#      True  and      True
+#           True
+
+print(2 != 5 and 3 > 4  or 2 <= 14  )
+
+#      True  and False    or True
+#      False              or True
+#           True
