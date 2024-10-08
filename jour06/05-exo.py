a7d2354088f47277841b45346a9e863e88b1fdad
Malik H <malik.h@webdevpro.net>
2024-10-08 12:41:25 +0200

    fin correction

--- jour06/05-exo.py
+++ jour06/05-exo.py
@@ -31,3 +31,8 @@ anagramme('finder', 'friend')
 anagramme('hello', 'bye')
 anagramme('mary', 'army') 
 anagramme('nectar', 'carnet') 
+
+set1 = { 1, 2 , 3 }
+set2 = { 3, 4 , 5 }
+print(set1.difference(set2))
+print(set1.symmetric_difference(set2))
