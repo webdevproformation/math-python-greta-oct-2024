c76ebf69606ec6b7164bfdc1ea5b98aa036fdce7
Malik H <malik.h@webdevpro.net>
2024-10-14 15:52:25 +0200

    cas pratique

--- jour09/11-exo.py
+++ jour09/11-exo.py
@@ -11,3 +11,35 @@ exo2 = Exo(4 , "instancier une class" , 2)
 
 print(exo1.note) # 3
 print(exo2.note) # 2
+
+"""
+créer le fichier 12-exo.py
+
+# class Article
+# 4 propriétés publiques
+# auteur = string
+# contenu = string
+# like = chiffre entier
+# titre = string 
+
+# cette class dispose d'une fonction constructrice __init__() ayant autant de paramètre que l'on a de propriétés dans la class  + self
+
+# va initialiser les propriétés de la class
+
+# ajouter une méthode pour cette class => render 
+# print concatenation via les  propriétés de la class du texte suivant
+/*
+<h1>titre</h1>
+<p>contenu</p>
+<ul>
+    <li>écrit par : auteur</li>
+    <li>like : like </li>
+</ul>
+*/
+# générer deux articles => 
+# article1  => valeurs des paramètres = "moi" / "lorem ipsum" / 10 / "mon premier article" 
+# article2  => valeurs des paramètres = "un étudiants" / "exemple article" / 15 / "utiliser une class au quotidien" 
+
+exécuter la méthode render pour l'article1 et l'article2
+
+"""
