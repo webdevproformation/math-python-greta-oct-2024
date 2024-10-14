cbb102eaf1529230c94c09c6f3f304555ebb5b5a
Malik H <malik.h@webdevpro.net>
2024-10-14 15:29:23 +0200

    dernière remarque sur le constructeur

--- jour09/11-exo.py
+++ jour09/11-exo.py
@@ -5,6 +5,8 @@ class Exo :
         self.duree = p_duree
 
 exo1 = Exo(5 , "créer une class" , 3)
+# Exo(5 , "créer une class" , 3) de manière automatique Python va rechercher SI il y a une méthode __init__ dans la class 
+# si elle existe bien il l'exécute 
 exo2 = Exo(4 , "instancier une class" , 2)
 
 print(exo1.note) # 3
