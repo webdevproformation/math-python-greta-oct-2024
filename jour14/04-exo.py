90e19b9ec305402f1b2696ab62c28a6586579545
Malik H <malik.h@webdevpro.net>
2024-10-29 14:16:52 +0100

    correction

---
+++ jour14/04-exo.py
@@ -0,0 +1,18 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__ (self, other):
+        return Point(self.x + other.x , self.y + other.y)
+    
+    def __str__(self) :
+        # return str(vars(self))
+        return f"Point(x={self.x}, y={self.y})"
+
+
+pt1 = Point(1, 2)
+pt2 = Point(3, 4)
+pt3 = pt1 + pt2
+print(pt3)
+
